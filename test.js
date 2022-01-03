4d95acc27da5fa6d12523d80e2d2bae35799eb92
YOUNG <957355473@qq.com>
2022-01-03 17:13:56 +0800

    sign-in

---
+++ test.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+//读数据库
+const usersString = fs.readFileSync("./db/users.json").toString();
+const usersArray = JSON.parse(usersString);
+console.log(usersArray);
+//写数据库
+const ysx = { id: 3, name: "zya", password: "eee" };
+usersArray.push(ysx);
+const string = JSON.stringify(usersArray);
+fs.writeFileSync("./db/users.json", string);
